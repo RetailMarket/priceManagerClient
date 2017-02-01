91b04fbb7776c5c14482c315538afe910d726608
Akshay <kumarsharmaa69@gmail.com>
2017-02-01 14:15:23 +0530

    Changed the method params

--- priceManager.pb.go
+++ priceManager.pb.go
@@ -10,10 +10,10 @@ It is generated from these files:
 
 It has these top-level messages:
 	Entry
-	RecordRequest
-	RecordResponse
-	RecordsRequest
-	RecordsResponse
+	Record
+	Request
+	Records
+	Response
 */
 package priceManagerClient
 
@@ -93,72 +93,64 @@ func (m *Entry) GetIsLatest() bool {
 	return false
 }
 
-type RecordRequest struct {
+type Record struct {
 	ProductId int32 `protobuf:"varint,1,opt,name=product_id,json=productId" json:"product_id,omitempty"`
 	Cost      int32 `protobuf:"varint,2,opt,name=cost" json:"cost,omitempty"`
 }
 
-func (m *RecordRequest) Reset()                    { *m = RecordRequest{} }
-func (m *RecordRequest) String() string            { return proto.CompactTextString(m) }
-func (*RecordRequest) ProtoMessage()               {}
-func (*RecordRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (m *Record) Reset()                    { *m = Record{} }
+func (m *Record) String() string            { return proto.CompactTextString(m) }
+func (*Record) ProtoMessage()               {}
+func (*Record) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
-func (m *RecordRequest) GetProductId() int32 {
+func (m *Record) GetProductId() int32 {
 	if m != nil {
 		return m.ProductId
 	}
 	return 0
 }
 
-func (m *RecordRequest) GetCost() int32 {
+func (m *Record) GetCost() int32 {
 	if m != nil {
 		return m.Cost
 	}
 	return 0
 }
 
-type RecordResponse struct {
-	Message string `protobuf:"bytes,1,opt,name=message" json:"message,omitempty"`
+type Request struct {
 }
 
-func (m *RecordResponse) Reset()                    { *m = RecordResponse{} }
-func (m *RecordResponse) String() string            { return proto.CompactTextString(m) }
-func (*RecordResponse) ProtoMessage()               {}
-func (*RecordResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+func (m *Request) Reset()                    { *m = Request{} }
+func (m *Request) String() string            { return proto.CompactTextString(m) }
+func (*Request) ProtoMessage()               {}
+func (*Request) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
-func (m *RecordResponse) GetMessage() string {
-	if m != nil {
-		return m.Message
-	}
-	return ""
-}
-
-type RecordsRequest struct {
+type Records struct {
 	Entries []*Entry `protobuf:"bytes,1,rep,name=entries" json:"entries,omitempty"`
 }
 
-func (m *RecordsRequest) Reset()                    { *m = RecordsRequest{} }
-func (m *RecordsRequest) String() string            { return proto.CompactTextString(m) }
-func (*RecordsRequest) ProtoMessage()               {}
-func (*RecordsRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
+func (m *Records) Reset()                    { *m = Records{} }
+func (m *Records) String() string            { return proto.CompactTextString(m) }
+func (*Records) ProtoMessage()               {}
+func (*Records) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
 
-func (m *RecordsRequest) GetEntries() []*Entry {
+func (m *Records) GetEntries() []*Entry {
 	if m != nil {
 		return m.Entries
 	}
 	return nil
 }
 
-type RecordsResponse struct {
+type Response struct {
 	Message string `protobuf:"bytes,1,opt,name=message" json:"message,omitempty"`
 }
 
-func (m *RecordsResponse) Reset()                    { *m = RecordsResponse{} }
-func (m *RecordsResponse) String() string            { return proto.CompactTextString(m) }
-func (*RecordsResponse) ProtoMessage()               {}
-func (*RecordsResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
+func (m *Response) Reset()                    { *m = Response{} }
+func (m *Response) String() string            { return proto.CompactTextString(m) }
+func (*Response) ProtoMessage()               {}
+func (*Response) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
 
-func (m *RecordsResponse) GetMessage() string {
+func (m *Response) GetMessage() string {
 	if m != nil {
 		return m.Message
 	}
@@ -167,10 +159,10 @@ func (m *RecordsResponse) GetMessage() string {
 
 func init() {
 	proto.RegisterType((*Entry)(nil), "priceManagerClient.Entry")
-	proto.RegisterType((*RecordRequest)(nil), "priceManagerClient.RecordRequest")
-	proto.RegisterType((*RecordResponse)(nil), "priceManagerClient.RecordResponse")
-	proto.RegisterType((*RecordsRequest)(nil), "priceManagerClient.RecordsRequest")
-	proto.RegisterType((*RecordsResponse)(nil), "priceManagerClient.RecordsResponse")
+	proto.RegisterType((*Record)(nil), "priceManagerClient.Record")
+	proto.RegisterType((*Request)(nil), "priceManagerClient.Request")
+	proto.RegisterType((*Records)(nil), "priceManagerClient.Records")
+	proto.RegisterType((*Response)(nil), "priceManagerClient.Response")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -184,11 +176,11 @@ const _ = grpc.SupportPackageIsVersion4
 // Client API for PriceManager service
 
 type PriceManagerClient interface {
-	PendingRecords(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error)
-	NotifyRecordsPicked(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error)
-	NotifyRecordsProcessed(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error)
-	LatestRecords(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error)
-	InsertRecord(ctx context.Context, in *RecordRequest, opts ...grpc.CallOption) (*RecordResponse, error)
+	PendingRecords(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Records, error)
+	NotifyRecordsPicked(ctx context.Context, in *Records, opts ...grpc.CallOption) (*Response, error)
+	NotifyRecordsProcessed(ctx context.Context, in *Records, opts ...grpc.CallOption) (*Response, error)
+	LatestRecords(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Records, error)
+	InsertRecord(ctx context.Context, in *Record, opts ...grpc.CallOption) (*Response, error)
 }
 
 type priceManagerClient struct {
@@ -199,8 +191,8 @@ func NewPriceManagerClient(cc *grpc.ClientConn) PriceManagerClient {
 	return &priceManagerClient{cc}
 }
 
-func (c *priceManagerClient) PendingRecords(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error) {
-	out := new(RecordsResponse)
+func (c *priceManagerClient) PendingRecords(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Records, error) {
+	out := new(Records)
 	err := grpc.Invoke(ctx, "/priceManagerClient.PriceManager/PendingRecords", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
@@ -208,8 +200,8 @@ func (c *priceManagerClient) PendingRecords(ctx context.Context, in *RecordsRequ
 	return out, nil
 }
 
-func (c *priceManagerClient) NotifyRecordsPicked(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error) {
-	out := new(RecordsResponse)
+func (c *priceManagerClient) NotifyRecordsPicked(ctx context.Context, in *Records, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
 	err := grpc.Invoke(ctx, "/priceManagerClient.PriceManager/NotifyRecordsPicked", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
@@ -217,8 +209,8 @@ func (c *priceManagerClient) NotifyRecordsPicked(ctx context.Context, in *Record
 	return out, nil
 }
 
-func (c *priceManagerClient) NotifyRecordsProcessed(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error) {
-	out := new(RecordsResponse)
+func (c *priceManagerClient) NotifyRecordsProcessed(ctx context.Context, in *Records, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
 	err := grpc.Invoke(ctx, "/priceManagerClient.PriceManager/NotifyRecordsProcessed", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
@@ -226,8 +218,8 @@ func (c *priceManagerClient) NotifyRecordsProcessed(ctx context.Context, in *Rec
 	return out, nil
 }
 
-func (c *priceManagerClient) LatestRecords(ctx context.Context, in *RecordsRequest, opts ...grpc.CallOption) (*RecordsResponse, error) {
-	out := new(RecordsResponse)
+func (c *priceManagerClient) LatestRecords(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Records, error) {
+	out := new(Records)
 	err := grpc.Invoke(ctx, "/priceManagerClient.PriceManager/LatestRecords", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
@@ -235,8 +227,8 @@ func (c *priceManagerClient) LatestRecords(ctx context.Context, in *RecordsReque
 	return out, nil
 }
 
-func (c *priceManagerClient) InsertRecord(ctx context.Context, in *RecordRequest, opts ...grpc.CallOption) (*RecordResponse, error) {
-	out := new(RecordResponse)
+func (c *priceManagerClient) InsertRecord(ctx context.Context, in *Record, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
 	err := grpc.Invoke(ctx, "/priceManagerClient.PriceManager/InsertRecord", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
@@ -247,11 +239,11 @@ func (c *priceManagerClient) InsertRecord(ctx context.Context, in *RecordRequest
 // Server API for PriceManager service
 
 type PriceManagerServer interface {
-	PendingRecords(context.Context, *RecordsRequest) (*RecordsResponse, error)
-	NotifyRecordsPicked(context.Context, *RecordsRequest) (*RecordsResponse, error)
-	NotifyRecordsProcessed(context.Context, *RecordsRequest) (*RecordsResponse, error)
-	LatestRecords(context.Context, *RecordsRequest) (*RecordsResponse, error)
-	InsertRecord(context.Context, *RecordRequest) (*RecordResponse, error)
+	PendingRecords(context.Context, *Request) (*Records, error)
+	NotifyRecordsPicked(context.Context, *Records) (*Response, error)
+	NotifyRecordsProcessed(context.Context, *Records) (*Response, error)
+	LatestRecords(context.Context, *Request) (*Records, error)
+	InsertRecord(context.Context, *Record) (*Response, error)
 }
 
 func RegisterPriceManagerServer(s *grpc.Server, srv PriceManagerServer) {
@@ -259,7 +251,7 @@ func RegisterPriceManagerServer(s *grpc.Server, srv PriceManagerServer) {
 }
 
 func _PriceManager_PendingRecords_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RecordsRequest)
+	in := new(Request)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -271,13 +263,13 @@ func _PriceManager_PendingRecords_Handler(srv interface{}, ctx context.Context, 
 		FullMethod: "/priceManagerClient.PriceManager/PendingRecords",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PriceManagerServer).PendingRecords(ctx, req.(*RecordsRequest))
+		return srv.(PriceManagerServer).PendingRecords(ctx, req.(*Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _PriceManager_NotifyRecordsPicked_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RecordsRequest)
+	in := new(Records)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -289,13 +281,13 @@ func _PriceManager_NotifyRecordsPicked_Handler(srv interface{}, ctx context.Cont
 		FullMethod: "/priceManagerClient.PriceManager/NotifyRecordsPicked",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PriceManagerServer).NotifyRecordsPicked(ctx, req.(*RecordsRequest))
+		return srv.(PriceManagerServer).NotifyRecordsPicked(ctx, req.(*Records))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _PriceManager_NotifyRecordsProcessed_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RecordsRequest)
+	in := new(Records)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -307,13 +299,13 @@ func _PriceManager_NotifyRecordsProcessed_Handler(srv interface{}, ctx context.C
 		FullMethod: "/priceManagerClient.PriceManager/NotifyRecordsProcessed",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PriceManagerServer).NotifyRecordsProcessed(ctx, req.(*RecordsRequest))
+		return srv.(PriceManagerServer).NotifyRecordsProcessed(ctx, req.(*Records))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _PriceManager_LatestRecords_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RecordsRequest)
+	in := new(Request)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -325,13 +317,13 @@ func _PriceManager_LatestRecords_Handler(srv interface{}, ctx context.Context, d
 		FullMethod: "/priceManagerClient.PriceManager/LatestRecords",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PriceManagerServer).LatestRecords(ctx, req.(*RecordsRequest))
+		return srv.(PriceManagerServer).LatestRecords(ctx, req.(*Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _PriceManager_InsertRecord_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RecordRequest)
+	in := new(Record)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -343,7 +335,7 @@ func _PriceManager_InsertRecord_Handler(srv interface{}, ctx context.Context, de
 		FullMethod: "/priceManagerClient.PriceManager/InsertRecord",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PriceManagerServer).InsertRecord(ctx, req.(*RecordRequest))
+		return srv.(PriceManagerServer).InsertRecord(ctx, req.(*Record))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -380,28 +372,28 @@ var _PriceManager_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("priceManager.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 364 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xb4, 0x93, 0xcd, 0x6a, 0xdb, 0x40,
-	0x10, 0x80, 0x2b, 0xff, 0x6b, 0xfc, 0x53, 0xd8, 0x82, 0xd9, 0xb6, 0x14, 0xe4, 0xed, 0x45, 0xb4,
-	0xe0, 0x83, 0xfd, 0x06, 0x2d, 0x3e, 0x18, 0x5a, 0x63, 0x74, 0x29, 0x34, 0x07, 0x47, 0x91, 0x26,
-	0x66, 0x89, 0xbd, 0xab, 0xec, 0xac, 0x03, 0x7e, 0xa7, 0xbc, 0x57, 0x5e, 0x23, 0x64, 0xad, 0x4d,
-	0x62, 0x92, 0xd8, 0x17, 0xe7, 0xa6, 0xf9, 0xfb, 0xbe, 0xd1, 0x20, 0x01, 0x2b, 0x8c, 0xcc, 0xf0,
-	0x6f, 0xaa, 0xd2, 0x25, 0x9a, 0x61, 0x61, 0xb4, 0xd5, 0x6c, 0x2f, 0xf7, 0x7b, 0x25, 0x51, 0x59,
-	0x71, 0x1b, 0x40, 0x7d, 0xa2, 0xac, 0xd9, 0xb2, 0x6f, 0x00, 0x85, 0xd1, 0xf9, 0x26, 0xb3, 0x0b,
-	0x99, 0xf3, 0x20, 0x0a, 0xe2, 0x7a, 0x12, 0x96, 0x99, 0x69, 0xce, 0x38, 0x34, 0x6f, 0xd0, 0x90,
-	0xd4, 0x8a, 0x57, 0xa2, 0x20, 0x0e, 0x13, 0x1f, 0xb2, 0x01, 0x74, 0xfc, 0xa0, 0x4a, 0xd7, 0xc8,
-	0xab, 0xae, 0xdc, 0x2e, 0x73, 0xb3, 0x74, 0x8d, 0x8c, 0x41, 0x2d, 0xd3, 0x64, 0x79, 0xcd, 0x51,
-	0xdd, 0x33, 0xeb, 0x43, 0x83, 0x6c, 0x6a, 0x37, 0xc4, 0xeb, 0x6e, 0xa0, 0x8c, 0xd8, 0x57, 0x08,
-	0x25, 0x2d, 0x56, 0xa9, 0x45, 0xb2, 0xbc, 0x11, 0x05, 0x71, 0x2b, 0x69, 0x49, 0xfa, 0xe3, 0x62,
-	0xf1, 0x0b, 0xba, 0x09, 0x66, 0xda, 0xe4, 0x09, 0x5e, 0x6f, 0x90, 0xec, 0xb1, 0xad, 0xbd, 0xb8,
-	0xf2, 0x24, 0x16, 0x3f, 0xa0, 0xe7, 0x19, 0x54, 0x68, 0x45, 0xf8, 0xf0, 0x6e, 0x6b, 0x24, 0x4a,
-	0x97, 0xe8, 0x08, 0x61, 0xe2, 0x43, 0x31, 0xf1, 0xbd, 0xe4, 0x85, 0x63, 0x68, 0xa2, 0xb2, 0x46,
-	0x22, 0xf1, 0x20, 0xaa, 0xc6, 0xed, 0xd1, 0xe7, 0xe1, 0xcb, 0xb3, 0x0e, 0xdd, 0x49, 0x13, 0xdf,
-	0x29, 0x7e, 0xc2, 0xc7, 0x47, 0xcc, 0x31, 0xe7, 0xe8, 0xae, 0x0a, 0x9d, 0xf9, 0x33, 0x24, 0x3b,
-	0x83, 0xde, 0x1c, 0x55, 0x2e, 0xd5, 0xb2, 0x84, 0x30, 0xf1, 0x9a, 0x73, 0x7f, 0xd1, 0x2f, 0xdf,
-	0x0f, 0xf6, 0xec, 0xb6, 0x10, 0x1f, 0xd8, 0x39, 0x7c, 0x9a, 0x69, 0x2b, 0x2f, 0xb7, 0x65, 0x69,
-	0x2e, 0xb3, 0x2b, 0xcc, 0x4f, 0x69, 0xc8, 0xa0, 0xbf, 0x6f, 0x30, 0x3a, 0x43, 0xa2, 0xd3, 0x4a,
-	0xfe, 0x43, 0x77, 0xf7, 0x89, 0xbc, 0xc3, 0x89, 0xfe, 0x41, 0x67, 0xaa, 0x08, 0x4d, 0xc9, 0x66,
-	0x83, 0xb7, 0xc7, 0x3c, 0x59, 0x1c, 0x6a, 0xf1, 0xe0, 0x8b, 0x86, 0xfb, 0x2f, 0xc7, 0xf7, 0x01,
-	0x00, 0x00, 0xff, 0xff, 0xfc, 0x59, 0x35, 0xe1, 0xad, 0x03, 0x00, 0x00,
+	// 361 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xac, 0x53, 0xcd, 0x6e, 0xda, 0x40,
+	0x10, 0xc6, 0xfc, 0x18, 0x3c, 0xd0, 0x1e, 0xb6, 0x12, 0xda, 0x42, 0x2b, 0xb9, 0x56, 0x0f, 0x3e,
+	0x71, 0x80, 0x63, 0xa5, 0x5e, 0xaa, 0x1e, 0x48, 0x08, 0x42, 0x2b, 0xe5, 0x8c, 0x1c, 0x7b, 0x82,
+	0x56, 0x81, 0x5d, 0x67, 0x67, 0x89, 0xc4, 0xdb, 0xe4, 0x01, 0xf2, 0x90, 0x91, 0x17, 0x5b, 0x09,
+	0x0a, 0x84, 0x43, 0x72, 0xdb, 0xef, 0x9b, 0xfd, 0x7e, 0x76, 0x2c, 0x03, 0xcb, 0x8d, 0x4c, 0xf1,
+	0x2a, 0x51, 0xc9, 0x0a, 0xcd, 0x28, 0x37, 0xda, 0x6a, 0x76, 0xc0, 0xfd, 0x5b, 0x4b, 0x54, 0x36,
+	0x7a, 0xf2, 0xa0, 0xf5, 0x5f, 0x59, 0xb3, 0x63, 0x3f, 0x01, 0x72, 0xa3, 0xb3, 0x6d, 0x6a, 0x97,
+	0x32, 0xe3, 0x5e, 0xe8, 0xc5, 0x2d, 0x11, 0x94, 0xcc, 0x34, 0x63, 0x1c, 0xda, 0x0f, 0x68, 0x48,
+	0x6a, 0xc5, 0xeb, 0xa1, 0x17, 0x07, 0xa2, 0x82, 0xec, 0x17, 0xf4, 0x2a, 0xa1, 0x4a, 0x36, 0xc8,
+	0x1b, 0x6e, 0xdc, 0x2d, 0xb9, 0x79, 0xb2, 0x41, 0xc6, 0xa0, 0x99, 0x6a, 0xb2, 0xbc, 0xe9, 0x5c,
+	0xdd, 0x99, 0xf5, 0xc1, 0x27, 0x9b, 0xd8, 0x2d, 0xf1, 0x96, 0x13, 0x94, 0x88, 0x0d, 0x21, 0x90,
+	0xb4, 0x5c, 0x27, 0x16, 0xc9, 0x72, 0x3f, 0xf4, 0xe2, 0x8e, 0xe8, 0x48, 0x9a, 0x39, 0x1c, 0xfd,
+	0x01, 0x5f, 0x60, 0xaa, 0x4d, 0x76, 0xae, 0x6e, 0x95, 0x58, 0x7f, 0x49, 0x8c, 0x02, 0x68, 0x0b,
+	0xbc, 0xdf, 0x16, 0x3e, 0x7f, 0x8b, 0x63, 0xe1, 0x43, 0x6c, 0x02, 0x6d, 0x54, 0xd6, 0x48, 0x24,
+	0xee, 0x85, 0x8d, 0xb8, 0x3b, 0xfe, 0x3e, 0x7a, 0xbb, 0xa7, 0x91, 0xdb, 0x91, 0xa8, 0x6e, 0x46,
+	0xbf, 0xa1, 0x23, 0x90, 0x72, 0xad, 0x08, 0x8b, 0xcd, 0x6c, 0x90, 0x28, 0x59, 0xa1, 0xab, 0x11,
+	0x88, 0x0a, 0x8e, 0x1f, 0x1b, 0xd0, 0x5b, 0xbc, 0xf2, 0x62, 0x33, 0xf8, 0xba, 0x40, 0x95, 0x49,
+	0xb5, 0xaa, 0xd2, 0x87, 0xc7, 0xc2, 0xca, 0x96, 0x83, 0x13, 0x43, 0xa7, 0x8c, 0x6a, 0x4c, 0xc0,
+	0xb7, 0xb9, 0xb6, 0xf2, 0x76, 0x57, 0x52, 0x0b, 0x99, 0xde, 0x61, 0xc6, 0xde, 0x53, 0x0d, 0x7e,
+	0x1c, 0x1f, 0xee, 0x9f, 0x12, 0xd5, 0xd8, 0x35, 0xf4, 0x0f, 0x3d, 0x8d, 0x4e, 0x91, 0xe8, 0xa3,
+	0xb6, 0x97, 0xf0, 0x65, 0xff, 0x05, 0x3f, 0xe3, 0xdd, 0x17, 0xd0, 0x9b, 0x2a, 0x42, 0x53, 0x9a,
+	0xb1, 0xc1, 0xe9, 0xeb, 0xe7, 0x8a, 0xdd, 0xf8, 0xee, 0xd7, 0x98, 0x3c, 0x07, 0x00, 0x00, 0xff,
+	0xff, 0xd5, 0x8d, 0xa8, 0x54, 0x30, 0x03, 0x00, 0x00,
 }
